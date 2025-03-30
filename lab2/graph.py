3364c5d7a1db19e06ca432f90ad012d8f99256d8
01trisha <daniltrishkin5@gmail.com>
2025-03-31 03:14:20 +0700

    Update graph

--- lab2/graph.py
+++ lab2/graph.py
@@ -12,19 +12,22 @@ omp_parallel_for_times = [68.392633, 33.878401, 19.211936, 9.737794, 8.764055]
 # Время работы последовательной программы
 T1 = 67.404712
 
-# Расчёт ускорения (Sp = T1 / Tp)
-sp_parallel = [T1 / time for time in omp_parallel_times]
-sp_parallel_for = [T1 / time for time in omp_parallel_for_times]
+# Создание графиков
+plt.figure(figsize=(10, 12))
 
-# Расчёт эффективности (Ep = Sp / p * 100%)
-ep_parallel = [sp / p * 100 for sp, p in zip(sp_parallel, threads)]
-ep_parallel_for = [sp / p * 100 for sp, p in zip(sp_parallel_for, threads)]
-
-# Создание графика
-plt.figure(figsize=(10, 6))
+# График времени выполнения (новый график, первый по порядку)
+plt.subplot(3, 1, 1)
+plt.plot(threads, omp_parallel_times, label='omp_parallel', marker='o', color='b')
+plt.plot(threads, omp_parallel_for_times, label='omp_parallel_for', marker='s', color='r')
+plt.title('Зависимость времени выполнения от количества потоков')
+plt.xlabel('Количество потоков')
+plt.ylabel('Время выполнения (сек)')
+plt.legend()
 
 # График ускорения
-plt.subplot(2, 1, 1)
+plt.subplot(3, 1, 2)
+sp_parallel = [T1 / time for time in omp_parallel_times]
+sp_parallel_for = [T1 / time for time in omp_parallel_for_times]
 plt.plot(threads, sp_parallel, label='omp_parallel', marker='o', color='b')
 plt.plot(threads, sp_parallel_for, label='omp_parallel_for', marker='s', color='r')
 plt.title('Ускорение от количества потоков')
@@ -33,7 +36,9 @@ plt.ylabel('Ускорение (Sp)')
 plt.legend()
 
 # График эффективности
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 3)
+ep_parallel = [sp / p * 100 for sp, p in zip(sp_parallel, threads)]
+ep_parallel_for = [sp / p * 100 for sp, p in zip(sp_parallel_for, threads)]
 plt.plot(threads, ep_parallel, label='omp_parallel', marker='o', color='b')
 plt.plot(threads, ep_parallel_for, label='omp_parallel_for', marker='s', color='r')
 plt.title('Эффективность от количества потоков')
